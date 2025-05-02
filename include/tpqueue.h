8f4440042cf32a61774a989acd4a273c41711587
Nastya3456-fed <Nasty3256@yandex.ru>
2025-05-03 00:49:33 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,23 @@
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TPQUEUE_H_
-#define INCLUDE_TPQUEUE_H_
-
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+class TPQueue {
+public:
+    TPQueue();
+    ~TPQueue();
+
+    void push(const SYM& ch);
+    SYM pop();
+    bool empty() const;
+
+private:
+    struct Node {
+        SYM data;
+        Node* next;
+        Node(const SYM& ch);
+    };
+
+    Node* head;
+};
