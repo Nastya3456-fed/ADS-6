5901e638ce11dec203e77338409c3ba6021a2460
Nastya3456-fed <Nasty3256@yandex.ru>
2025-05-03 02:01:08 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -6,24 +6,62 @@ struct SYM {
     int prior;
 };
 
+
+bool operator<(const SYM& lhs, const SYM& rhs) {
+    return lhs.prior < rhs.prior;
+}
+
+
 template<typename T>
 class TPQueue {
-public:
-    TPQueue();
-    ~TPQueue();
-
-    void push(const T& ch);
-    T pop();
-    bool empty() const;
-
-private:
     struct Node {
         T data;
         Node* next;
-        Node(const T& ch);
+        Node(const T& d) : data(d), next(nullptr) {}
     };
 
     Node* head;
+
+public:
+    TPQueue() : head(nullptr) {}
+
+    ~TPQueue() {
+        while (!empty()) {
+            pop();
+        }
+    }
+
+    bool empty() const {
+        return head == nullptr;
+    }
+
+    void push(const T& element) {
+        Node* newNode = new Node(element);
+
+        if (!head || element < head->data) {
+            newNode->next = head;
+            head = newNode;
+        }
+        else {
+            Node* current = head;
+            while (current->next && !(element < current->next->data)) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+
+    T pop() {
+        if (empty()) {
+            throw std::out_of_range("pop from empty queue");
+        }
+        Node* temp = head;
+        T result = temp->data;
+        head = head->next;
+        delete temp;
+        return result;
+    }
 };
 
 #endif
