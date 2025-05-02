e3751c48e3d5fc3b46cefb4537a31e632f9deec8
Nastya3456-fed <Nasty3256@yandex.ru>
2025-05-03 02:15:51 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,66 +4,56 @@
 #include <stdexcept>
 
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
 
-
 bool operator<(const SYM& lhs, const SYM& rhs) {
-    return lhs.prior > rhs.prior;
+  return lhs.prior > rhs.prior;
 }
-
-
 template<typename T>
 class TPQueue {
-    struct Node {
-        T data;
-        Node* next;
-        Node(const T& d) : data(d), next(nullptr) {}
-    };
-
-    Node* head;
-
+  struct Node {
+    T data;
+    Node* next;
+    Node(const T& d) : data(d), next(nullptr) {}
+  };
+Node* head;
 public:
-    TPQueue() : head(nullptr) {}
-
-    ~TPQueue() {
-        while (!empty()) {
-            pop();
-        }
+  TPQueue() : head(nullptr) {}
+  ~TPQueue() {
+    while (!empty()) {
+      pop();
     }
-
-    bool empty() const {
-        return head == nullptr;
+  }
+  bool empty() const {
+    return head == nullptr;
+  }
+  void push(const T& element) {
+    Node* newNode = new Node(element);
+    if (!head || element < head->data) {
+      newNode->next = head;
+      head = newNode;
     }
-
-    void push(const T& element) {
-        Node* newNode = new Node(element);
-
-        if (!head || element < head->data) {
-            newNode->next = head;
-            head = newNode;
-        }
-        else {
-            Node* current = head;
-            while (current->next && !(element < current->next->data)) {
-                current = current->next;
-            }
-            newNode->next = current->next;
-            current->next = newNode;
-        }
+    else {
+      Node* current = head;
+      while (current->next && !(element < current->next->data)) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
     }
-
-    T pop() {
-        if (empty()) {
-            throw std::out_of_range("pop from empty queue");
-        }
-        Node* temp = head;
-        T result = temp->data;
-        head = head->next;
-        delete temp;
-        return result;
+  }
+  T pop() {
+    if (empty()) {
+      throw std::out_of_range("pop from empty queue");
     }
+    Node* temp = head;
+    T result = temp->data;
+    head = head->next;
+    delete temp;
+    return result;
+  }
 };
 
 #endif
